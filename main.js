885879273882d2d293d4ef4a25be39b8f68c1cf6
Lisha03 <102582907+Lisha03@users.noreply.github.com>
2022-12-11 13:27:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+https://teachablemachine.withgoogle.com/models/tJyFzBGms/
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(dat_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    });
+
+}
+console.log('ml5 version:',ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/tJyFzBGms/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log('modelLoaded!');
+}
+function check()
+{
+    img=document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+function gotResult(error,result){
+    if(error){
+    }else{
+        console.log(result);
+        document.getElementById("result_object_name").innerHTML=results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML=results[0].cofidence.toFixed(3);
+    }
+}
